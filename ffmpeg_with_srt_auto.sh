5c860e6929a43644a64b564cb8b8802004345c7a
uf0wxxel <57374198+uf0wxxel@users.noreply.github.com>
2022-10-14 08:05:13 +0000

    Update ffmpeg_with_srt_auto.sh

--- ffmpeg_with_srt_auto.sh
+++ ffmpeg_with_srt_auto.sh
@@ -112,7 +112,7 @@ if [ "$libpulse_reponse" = "yes" ]; then
 	wget -O pulseaudio-16.1.tar.xz https://freedesktop.org/software/pulseaudio/releases/pulseaudio-16.1.tar.xz && \
 	tar xJvf pulseaudio-16.1.tar.xz && \
 	cd pulseaudio-16.1 && \
-	PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig" meson --prefix="$HOME/ffmpeg_build" build && \
+	PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig" meson --prefix="$HOME/ffmpeg_build" --man=false --tests=false build && \
 	ninja -C build install
 else
 	printf "\n\nlibpulse will be skipped\n\n"
